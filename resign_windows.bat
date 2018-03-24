b4625c222eb9d09b3e1ad42fecbc183c6bf66c31
Caio Oliveira <caiooliveirafarias0@gmail.com>
2018-03-23 23:43:51 -0300

    resign_windows.bat: Rewrite script to generate a PKG with all RIF and act.dat
    * It generate a PKG with all resigned RIF files and [signed_]act.dat
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- resign_windows.bat
+++ resign_windows.bat
@@ -1,40 +1,139 @@
 :: ------------------------------------------------------------------
 :: Simple script to build a proper PKG using Python (by CaptainCPS-X)
+:: Adapted to PS3xploit-ReSigner (by Caio99BR)
 :: ------------------------------------------------------------------
+:: Disable Debug output
 @echo off
-cd "%~dp0"
 
-:: Change this depending where you installed Python...
-set PYTHON=c:\Python27
+:: Save Current Working Dir
+set CURRENT_DIR="%cd%"
 
-:: Don't change these...
-set PATH=%PYTHON%;%PATH%
-set PKG=.\bin\pkg_exdata.exe
+:: Go to current dir
+cd "%CURRENT_DIR%"
 
-:: Change these for your application / manual...
-set CONTENTID=RIF000-INSTALLER_00-0000000000000000
-set PKG_DIR=./exdata/
-set PKG_NAME=./%CONTENTID%.pkg
+:: Main Tools
+set TOOLS_PKG_EXDATA="%cd%\source\tools\ps3py_exe\pkg_exdata.exe"
+set TOOLS_RESIGNER="%cd%\source\pre-compiled\windows\ps3xploit_rifgen_edatresign.exe"
 
-:: Resign all rap files
-for %%I in (./raps/*.rap) do (
-	if exist "./raps/%%~nI.rif" (
-		echo "File exist ./raps/%%~nI.rif"
+:: Output variables
+set OUTPUT_PKGS_DIR=output\pkgs
+set OUPUT_RIF_PKG_FILES=output\temp
+set OUPUT_RIF_PKG_NAME=output\rif_pkg\PKG_RIF-INSTALLER.pkg
+
+:: Input Dirs
+set INPUT_RAPS_DIR=input\raps
+set INPUT_PKGS_DIR=input\pkgs
+
+:: Input Files
+set INPUT_ACT_DAT=input\act_dat\act.dat
+set INPUT_IDPS_HEX=input\idps_hex\idps.hex
+
+:: RIF Package ContentID
+set RIF_PKG_CONTENTID=RIF000-INSTALLER_00-0000000000000000
+
+:: Cleanup before everything
+del %CURRENT_DIR%\%OUTPUT_PKGS_DIR%\*.pkg
+del %CURRENT_DIR%\%OUPUT_RIF_PKG_NAME%_signed.pkg
+
+:: Copy RAP files
+if not exist %CURRENT_DIR%\%INPUT_RAPS_DIR%\*.rap (
+	echo. 
+	echo ps3xploit_resign: No '.rap' files found on '.\%INPUT_RAPS_DIR%\'
+	echo. 
+	if exist %CURRENT_DIR%\%INPUT_PKGS_DIR%\*.pkg (
+		GOTO RESIGN_PKG_ONLY
 	) else (
-::		echo | ps3xploit_rifgen_edatresign_orig.exe "./raps/%%I"
-		ps3xploit_rifgen_edatresign.exe "%cd%\raps\%%I"
+		echo. 
+		echo ps3xploit_resign: No '.pkg' files found on '.\%INPUT_PKGS_DIR%\'
+		echo. 
+		pause
+		exit /b
 	)
 )
+copy /Y %CURRENT_DIR%\%INPUT_RAPS_DIR%\*.rap %CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%\
 
-pause
+:: Copy act.dat files
+if not exist %CURRENT_DIR%\%INPUT_ACT_DAT% (
+	echo. 
+	echo ps3xploit_resign: '.\%INPUT_ACT_DAT%' not found, exiting...
+	echo. 
+	pause
+	exit /b
+)
+copy /Y %CURRENT_DIR%\%INPUT_ACT_DAT% %CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%\
 
-if not exist "%cd%\exdata" mkdir "%cd%\exdata"
-copy /Y "%cd%\raps\*.rif" "%cd%\exdata\"
-copy /Y "%cd%\signed_act.dat" "%cd%\exdata\act.dat"
+:: Copy idps.hex files
+if not exist %CURRENT_DIR%\%INPUT_IDPS_HEX% (
+	echo. 
+	echo ps3xploit_resign: '.\%INPUT_IDPS_HEX%' not found, exiting...
+	echo. 
+	pause
+	exit /b
+)
+copy /Y %CURRENT_DIR%\%INPUT_IDPS_HEX% %CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%\
 
-%PKG% --contentid %CONTENTID% %PKG_DIR% %PKG_NAME%
-ps3xploit_rifgen_edatresign %PKG_NAME% ps3
-del "%PKG_NAME%"
-del "%cd%\raps\*rif"
+:: Resign all RAP files to RIF files
+del %CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%\*.rif
+for %%I in (%CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%\*.rap) do (
+	cd %CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%
+	%TOOLS_RESIGNER% %%I
+	cd %CURRENT_DIR%\
+)
 
+:: Delete unneed files on pkg
+del %CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%\*.rap
+del %CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%\act.dat
+del %CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%\idps.hex
+
+:: Move 'signed_act.dat' to 'act.dat'
+move %CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%\signed_act.dat %CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%\act.dat
+
+:: Build PKG RIF
+%TOOLS_PKG_EXDATA% --contentid %RIF_PKG_CONTENTID% %CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%\ %CURRENT_DIR%\%OUPUT_RIF_PKG_NAME%
+
+:: Resign PKG RIF
+if not exist %CURRENT_DIR%\%OUPUT_RIF_PKG_NAME% (
+	echo. 
+	echo ps3xploit_resign: '.\%OUPUT_RIF_PKG_NAME%' not found, exiting...
+	echo. 
+	pause
+	exit /b
+)
+%TOOLS_RESIGNER% %CURRENT_DIR%\%OUPUT_RIF_PKG_NAME%
+
+:: Cleanup
+del %CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%\*.rif
+del %CURRENT_DIR%\%OUPUT_RIF_PKG_FILES%\act.dat
+del %CURRENT_DIR%\%OUPUT_RIF_PKG_NAME%
+
+:: Resign PKG files
+:RESIGN_PKG_ONLY
+for %%I in (%CURRENT_DIR%\%INPUT_PKGS_DIR%\*.pkg) do (
+	%TOOLS_RESIGNER% %%I
+	move %%I_signed.pkg %CURRENT_DIR%\%OUTPUT_PKGS_DIR%\
+)
+
+:: Output header
+echo. 
+echo ps3xploit_resign: Output files:
+
+:: See RIF PKG
+if exist %CURRENT_DIR%\%OUPUT_RIF_PKG_NAME%_signed.pkg (
+	echo. 
+	echo.  RIF PKG:
+	echo.    .\%OUPUT_RIF_PKG_NAME%_signed.pkg
+	echo. 
+)
+
+:: See PKGS signed
+if exist %CURRENT_DIR%\%OUTPUT_PKGS_DIR%\*.pkg (
+	echo. 
+	echo.  PKGS:
+	for %%I in (%OUTPUT_PKGS_DIR%\*.pkg) do (
+		echo.    .\%%I
+	)
+	echo. 
+)
+
+:: Let user see everything
 pause
